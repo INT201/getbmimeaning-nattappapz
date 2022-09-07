d2779c898ac60582df602aed147f354d68518c73
Nattaphat <nattaphat.ngar@kmutt.ac.th>
2022-09-07 17:42:54 +0700

    bmi

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,21 @@
 const { template } = require('@babel/core')
-
+let bmi;
 function calculateBMI(weight, height) {
-  //code here
+    bmi = weight/Math.pow(height,2)
+    return bmi;
 }
 function getBMIMeaning(weight, height) {
-  //code here
+    const bmi1 = bmi
+    if(bmi1<18.5){
+        console.log("UnderWeight")
+    }
+    else if(bmi1>=18.5 && bmi1<=24.9){
+        console.log("Normal Weight") 
+    }
+    else if(bmi1 > 24.9){
+        console.log("Overweight") 
+    }
+  
 }
 module.exports = getBMIMeaning
+
